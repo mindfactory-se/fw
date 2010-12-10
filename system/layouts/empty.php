dedf241d3ecc67f8dd18be481bb77eb15f566db5
hepper <henke.persson@gmail.com>
2010-12-10 13:51:44 +0100

    Added an empty layout.

---
+++ system/layouts/empty.php
@@ -0,0 +1,13 @@
+<?php HTML::docType()?>
+
+<html>
+    <head>
+        <?php echo Html::charset()?>
+
+        <title></title>
+    </head>
+    <body>
+        <?php echo $content; ?>
+
+    </body>
+</html>
